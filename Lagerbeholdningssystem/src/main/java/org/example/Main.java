feb0f7d23da172f7cb4ec228816d400c6c754194
Tormod Mork Müller <tormod.muller@norgesgruppen.no>
2024-09-05 13:26:15 +0200

    Add reusable scanner

--- Lagerbeholdningssystem/src/main/java/org/example/Main.java
+++ Lagerbeholdningssystem/src/main/java/org/example/Main.java
@@ -4,6 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Method");
+        InputHelper inputHelper = new InputHelper();
+
+        String name = inputHelper.getString("Enter your name: ");
+        int age = inputHelper.getInt("Enter your age: ");
+
+        System.out.println("Hello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
+
+        inputHelper.close();
     }
 }
