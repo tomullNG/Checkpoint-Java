b248b356726bbd7b13902aa476f8ee39f2505475
Tormod Mork Müller <tormod.muller@norgesgruppen.no>
2024-09-06 11:00:42 +0200

    update user input

--- Lagerbeholdningssystem/src/main/java/org/example/Main.java
+++ Lagerbeholdningssystem/src/main/java/org/example/Main.java
@@ -6,16 +6,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        String lagerFil = "lagerbeholdning.json";
-        FileWriter fw = new FileWriter();
-        fw.UpdateStorage("A -20;C -5", lagerFil);
-
-
         // TODO: Add initializer of writing data to lagerbeholdning.json
-
-        FileReader fr = new FileReader();
-        //fr.ReadFile("lagerbeholdning.json");
-        fr.ReadOrderData("ordredata.json");
 
         UserInputHandler userInputHandler = new UserInputHandler();
         userInputHandler.openCommunication();
